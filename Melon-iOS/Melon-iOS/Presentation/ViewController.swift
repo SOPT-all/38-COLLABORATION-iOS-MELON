b39e23350e4ebb22bb32ad10e2aac21a4a8baa49
sangyup <sangyeobi712@gmail.com>
2026-05-09 01:57:30 +0900

    [Setting] 프로젝트 생성

---
+++ Melon-iOS/Melon-iOS/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Melon-iOS
+//
+//  Created by 이상엽 on 5/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
