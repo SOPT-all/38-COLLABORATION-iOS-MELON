a8f582f48f13ef525f111a025511fa499e68a7f4
sangyup <sangyeobi712@gmail.com>
2026-05-10 17:04:02 +0900

    [Setting] #3 BaseView, BaseViewController 생성

---
+++ Melon-iOS/Melon-iOS/Global/Base/BaseView.swift
@@ -0,0 +1,28 @@
+//
+//  BaseView.swift
+//  Melon-iOS
+//
+//  Created by 이상엽 on 5/10/26.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setStyle() {}
+    
+    func setUI() {}
+    
+    func setLayout() {}
+}
