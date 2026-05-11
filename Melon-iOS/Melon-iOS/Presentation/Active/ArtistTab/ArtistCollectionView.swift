aeaf796b54ae0b85ab46633dbef5dcfdf55c2598
seunghee <daalgwabyul@naver.com>
2026-05-11 18:38:53 +0900

    [Style] #12 - 컬렉션 뷰 구현 및 더미데이터 연결

--- Melon-iOS/Melon-iOS/Presentation/Active/ArtistTab/ArtistCollectionView.swift
+++ Melon-iOS/Melon-iOS/Presentation/Active/ArtistTab/ArtistCollectionView.swift
@@ -12,33 +12,63 @@ import Then
 
 final class ArtistCollectionView: UIView {
     
-    private let collectionViewLayout = UICollectionViewFlowLayout().then {
+    private let flowLayout = UICollectionViewFlowLayout().then {
+        $0.itemSize = CGSize(width: 100, height: 127)
         $0.scrollDirection = .horizontal
         $0.minimumInteritemSpacing = 10
-        $0.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-        $0.itemSize = CGSize(width: 100, height: 127)
     }
-    private lazy var collectionView: UICollectionView = {
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
-        cv.backgroundColor = .clear
-        cv.showsHorizontalScrollIndicator = false
-        cv.register(ArtistCell.self,forCellWithReuseIdentifier: ArtistCell.identifier)
-        return cv
-    }()
+    
+    private let imageList = ImageModel.dummy()
+    
+    private lazy var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: flowLayout
+    )
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .black
+        
+        backgroundColor = .appBlack
         addSubview(collectionView)
+        collectionView.backgroundColor = .clear
         collectionView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.height.equalTo(130)
+            $0.leading.equalToSuperview().inset(20)
+            $0.verticalEdges.equalToSuperview()
+            $0.trailing.equalToSuperview()
         }
+        collectionView.dataSource = self
+        register()
     }
+    
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    private func register() {
+        collectionView.register(
+            ArtistCell.self,forCellWithReuseIdentifier: ArtistCell.identifier
+        )
+    }
+}
+
+extension ArtistCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,numberOfItemsInSection section: Int) -> Int {
+        return imageList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ArtistCell.identifier, for: indexPath
+        ) as! ArtistCell
+        cell.dataBind(imageList[indexPath.row])
+        return cell
     }
 }
 
 #Preview {
-    ArtistCollectionView()
+    let view = ArtistCollectionView()
+    view.backgroundColor = .appBlack
+    view.frame = CGRect(x: 0, y: 0, width: 393, height: 130)
+    return view
 }
