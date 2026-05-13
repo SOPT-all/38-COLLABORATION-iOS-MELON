57e29bd2e6e44661969e14ba7f2a5faba1e0d471
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-05-13 21:13:37 +0900

    [Chore] #21 - 코드 리뷰 반영

--- Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/ChartCell.swift
+++ Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/ChartCell.swift
@@ -42,78 +42,79 @@ final class ChartCell: UICollectionViewCell {
             artistLabel.text = song.artistName
         }
         
-        private func setUI() {
-            contentView.addSubviews(
-                albumImageView, rankLabel, titleLabel,
-                rankChangeLabel, artistLabel, playButton
-            )
+    private func setUI() {
+        contentView.addSubviews(
+            albumImageView, rankLabel, titleLabel,
+            rankChangeLabel, artistLabel, playButton
+        )
+    }
+    
+    private func setLayout() {
+        albumImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(40)
         }
         
-        private func setLayout() {
-            albumImageView.snp.makeConstraints {
-                $0.top.equalToSuperview().inset(10)
-                $0.leading.equalToSuperview().inset(20)
-                $0.centerY.equalToSuperview()
-                $0.size.equalTo(40)
-            }
-            
-            rankLabel.snp.makeConstraints {
-                $0.leading.equalTo(albumImageView.snp.trailing).offset(19)
-                $0.top.equalToSuperview().inset(16.5)
-            }
-            
-            titleLabel.snp.makeConstraints {
-                $0.leading.equalTo(rankLabel.snp.trailing).offset(19)
-                $0.top.equalToSuperview().inset(14)
-            }
-            
-            rankChangeLabel.snp.makeConstraints {
-                $0.top.equalTo(rankLabel.snp.bottom).offset(8)
-                $0.leading.equalTo(albumImageView.snp.trailing).offset(19)
-            }
-            
-            artistLabel.snp.makeConstraints {
-                $0.leading.equalTo(rankLabel.snp.trailing).offset(19)
-                $0.top.equalTo(titleLabel.snp.bottom)
-            }
-            
-            playButton.snp.makeConstraints {
-                $0.centerY.equalToSuperview()
-                $0.trailing.equalToSuperview()
-                $0.size.equalTo(22)
-            }
+        rankLabel.snp.makeConstraints {
+            $0.leading.equalTo(albumImageView.snp.trailing).offset(19)
+            $0.top.equalToSuperview().inset(16.5)
         }
         
-        private func setStyle() {
-            albumImageView.do {
-                $0.layer.cornerRadius = 4
-                $0.clipsToBounds = true
-                $0.contentMode = .scaleAspectFill
-            }
-            
-            rankLabel.do {
-                $0.font = .body_r_15
-                $0.textColor = .appWhite
-            }
-            
-            titleLabel.do {
-                $0.font = .body_r_15
-                $0.textColor = .appWhite
-            }
-            
-            rankChangeLabel.do {
-                $0.font = .body_r_15
-                $0.textColor = .appWhite
-            }
-            
-            artistLabel.do {
-                $0.font = .body_r_13
-                $0.textColor = .gray300
-            }
-            
-            playButton.do {
-                $0.setImage(.icPlayFilled, for: .normal)
-                $0.tintColor = .appWhite
-            }
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(rankLabel.snp.trailing).offset(19)
+            $0.top.equalToSuperview().inset(14)
+        }
+        
+        rankChangeLabel.snp.makeConstraints {
+            $0.top.equalTo(rankLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(albumImageView.snp.trailing).offset(19)
+        }
+        
+        artistLabel.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.top.equalTo(titleLabel.snp.bottom)
+        }
+        
+        playButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.size.equalTo(22)
         }
     }
+    
+    private func setStyle() {
+        albumImageView.do {
+            $0.layer.cornerRadius = 4
+            $0.clipsToBounds = true
+            $0.contentMode = .scaleAspectFill
+        }
+        
+        rankLabel.do {
+            $0.font = .body_r_15
+            $0.textColor = .appWhite
+        }
+        
+        titleLabel.do {
+            $0.font = .body_r_15
+            $0.textColor = .appWhite
+        }
+        
+        rankChangeLabel.do {
+            $0.font = .body_r_15
+            $0.textColor = .appWhite
+        }
+        
+        artistLabel.do {
+            $0.font = .body_r_13
+            $0.textColor = .gray300
+        }
+        
+        playButton.do {
+            $0.setImage(.icPlayFilled, for: .normal)
+            $0.tintColor = .appWhite
+        }
+    }
+}
+
--- Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/ChartSectionView.swift
+++ Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/ChartSectionView.swift
@@ -12,6 +12,8 @@ import Then
 
 final class ChartSectionView: BaseView {
     
+    private let chartItems: [ChartSong] = ChartSongDTO.dummy
+    
     private let chartHeaderView = HomeDetailSectionHeaderView(subtitle: "오늘 16:00 기준", title: "실시간 트렌드 멜론차트", icon: .iconBrand3Purple)
     
     private let filterView = FilterBarView(items: ["1": "TOP100", "2": "HOT100", "3": "2006년 해외", "4": "달달한"], selectedID: "1")
@@ -19,8 +21,6 @@ final class ChartSectionView: BaseView {
     private let chartView = UICollectionView(frame: .zero, collectionViewLayout: ChartLayout.make())
 
     private let listenButton = ListenButton(title: "TOP100 전체 듣기")
-    
-    private let chartItems: [ChartSong] = ChartSongDTO.dummy
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -70,7 +70,6 @@ final class ChartSectionView: BaseView {
         chartView.do {
             $0.backgroundColor = .appBlack
             $0.showsHorizontalScrollIndicator = false
-            $0.dataSource = self
             $0.canCancelContentTouches = true
         }
     }
