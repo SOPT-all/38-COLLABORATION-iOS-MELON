44d9f684be08f85e84bd76796b162b4a70bcf9ef
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-05-14 01:54:30 +0900

    [Chore] #29 - 줄바꿈 수정

--- Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/EventLayout.swift
+++ Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/EventLayout.swift
@@ -9,17 +9,38 @@ import UIKit
 
 enum EventLayout {
     static func make() -> UICollectionViewLayout {
-        let wideItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(252), heightDimension: .absolute(188)))
-        
-        let narrowItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(143), heightDimension: .absolute(188)))
-        
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(407), heightDimension: .absolute(188)), subitems: [wideItem, narrowItem])
+        let wideItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(252),
+                heightDimension: .absolute(188)
+            )
+        )
+
+        let narrowItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(143),
+                heightDimension: .absolute(188)
+            )
+        )
+
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(407),
+                heightDimension: .absolute(188)
+            ),
+            subitems: [wideItem, narrowItem]
+        )
         group.interItemSpacing = .fixed(12)
-        
+
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .continuous
-        section.contentInsets = .init(top: 8, leading: 20, bottom: 8, trailing: 20)
-        
+        section.contentInsets = .init(
+            top: 8,
+            leading: 20,
+            bottom: 8,
+            trailing: 20
+        )
+
         return UICollectionViewCompositionalLayout(section: section)
     }
 }
