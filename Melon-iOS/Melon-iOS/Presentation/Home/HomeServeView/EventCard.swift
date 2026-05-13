805128a7414971f869a56b12d73567cf3b13d26d
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-05-13 21:04:03 +0900

    [Style] #29 - 이벤트 컬뷰 구현

---
+++ Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/EventCard.swift
@@ -0,0 +1,90 @@
+//
+//  EventCard.swift
+//  Melon-iOS
+//
+//  Created by h2e on 5/13/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class EventCard: UICollectionViewCell {
+    
+    static let identifier: String = "EventCard"
+    
+    private let imageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setLayout()
+        setStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with item: EventItem) {
+        imageView.image = item.image
+        titleLabel.text = item.title
+        subtitleLabel.text = item.subtitle
+    }
+    
+    private func setUI() {
+        addSubviews(imageView, titleLabel, subtitleLabel)
+    }
+    
+    private func setLayout() {
+        imageView.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+            $0.height.equalTo(141)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(10)
+            $0.leading.equalToSuperview()
+        }
+        
+        subtitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.leading.equalToSuperview()
+        }
+    }
+    
+    private func setStyle() {
+        imageView.do {
+            $0.layer.cornerRadius = 4
+        }
+        
+        titleLabel.do {
+            $0.font = .body_r_15
+            $0.textColor = .appWhite
+        }
+        
+        subtitleLabel.do {
+            $0.font = .caption_r_12
+            $0.textColor = .gray300
+        }
+    }
+}
+
+struct EventItem {
+    let image: UIImage
+    let title: String
+    let subtitle: String
+}
+
+extension EventItem {
+    static let dummy: [EventItem] = [
+        .init(image: UIImage(resource: .imgHomePromotion1),
+              title: "루프탑 파티: 국내 힙합",
+              subtitle: "#루프탑 #국내힙합"),
+        .init(image: UIImage(resource: .imgHomePromotion2), title: "루프탑 파티: 국내 힙합", subtitle: "#루프탑 #국내힙합")
+        ]
+}
---
+++ Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/EventSectionView.swift
@@ -0,0 +1,89 @@
+//
+//  EventSectionView.swift
+//  Melon-iOS
+//
+//  Created by h2e on 5/13/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class EventSectionView: BaseView {
+    private let eventHeaderView = HomeDetailSectionHeaderView(subtitle: "아티스트와 함께", title: "이벤트", icon: .iconBrand2Green)
+    
+    private let eventTitleLabel = UILabel()
+    
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: EventLayout.make())
+    
+    private let items: [EventItem] = EventItem.dummy
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setDelegate()
+        register()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    override func setUI() {
+        addSubviews(eventHeaderView, eventTitleLabel, collectionView)
+    }
+    
+    override func setLayout() {
+        eventHeaderView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+        
+        eventTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(eventHeaderView.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(eventTitleLabel.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(188)
+        }
+    }
+    
+    override func setStyle() {
+        eventTitleLabel.do {
+            $0.font = .body_sb_18
+            $0.textColor = .appWhite
+            $0.text = "지금 가장 기대되는 2026 페스티벌"
+        }
+        
+        collectionView.do {
+            $0.backgroundColor = .appBlack
+            $0.showsHorizontalScrollIndicator = false
+        }
+    }
+    
+    private func setDelegate() {
+        collectionView.dataSource = self
+    }
+    
+    private func register() {
+        collectionView.register(EventCard.self, forCellWithReuseIdentifier: EventCard.identifier)
+    }
+}
+
+extension EventSectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EventCard.identifier, for: indexPath) as? EventCard else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configure(with: items[indexPath.item])
+        return cell
+    }
+}
