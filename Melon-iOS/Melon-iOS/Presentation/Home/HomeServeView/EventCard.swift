5809e6c0ea30316ab572ccf1742ac7e1c6fb0e5b
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-05-13 23:50:08 +0900

    [Chore] #29 - 파일 분리

--- Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/EventCard.swift
+++ Melon-iOS/Melon-iOS/Presentation/Home/HomeServeView/EventCard.swift
@@ -76,18 +76,3 @@ extension EventCard {
         subtitleLabel.text = item.subtitle
     }
 }
-
-struct EventItem {
-    let image: UIImage
-    let title: String
-    let subtitle: String
-}
-
-extension EventItem {
-    static let dummy: [EventItem] = [
-        .init(image: UIImage(resource: .imgHomePromotion1),
-              title: "루프탑 파티: 국내 힙합",
-              subtitle: "#루프탑 #국내힙합"),
-        .init(image: UIImage(resource: .imgHomePromotion2), title: "루프탑 파티: 국내 힙합", subtitle: "#루프탑 #국내힙합")
-        ]
-}
