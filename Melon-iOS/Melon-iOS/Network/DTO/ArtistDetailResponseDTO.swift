a56e091a40d8a8b442227e67f980bea641cdde33
seunghee <daalgwabyul@naver.com>
2026-05-19 00:42:48 +0900

    [Network] 아티스트 상세정보 조회 DTO 작성

---
+++ Melon-iOS/Melon-iOS/Network/DTO/ArtistDetailResponseDTO.swift
@@ -0,0 +1,42 @@
+//
+//  ArtistDetailDTO.swift
+//  Melon-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 5/19/26.
+//
+import UIKit
+
+struct ArtistDetailResponseDTO: Decodable {
+    let artistId: Int
+    let name: String
+    let fanCount: Int
+    let commentCount: Int
+    let activeDegree: Int
+    let imageUrl: String
+    let type: ArtistType
+    let artists: [ArtistMemberDTO]
+    let recentAlbums: [RecentAlbumDTO]
+    let credit: String
+    let country: String
+    let agency: String
+    let awardHistory: [String]
+    let description: String
+}
+
+enum ArtistType: String, Decodable {
+    case solo = "SOLO"
+    case group = "GROUP"
+}
+
+struct ArtistMemberDTO: Decodable {
+    let artistId: Int
+    let name: String
+    let imageUrl: String
+}
+
+struct RecentAlbumDTO: Decodable {
+    let albumId: Int
+    let title: String
+    let artistName: String
+    let imageUrl: String
+}
