98f9cb2e5d0fd6940a7076831146085baed687ec
seunghee <daalgwabyul@naver.com>
2026-05-20 13:10:16 +0900

    [Network] 네트워크 서비스 호출 함수 작성

--- Melon-iOS/Melon-iOS/Presentation/Active/ActiveViewController.swift
+++ Melon-iOS/Melon-iOS/Presentation/Active/ActiveViewController.swift
@@ -11,7 +11,10 @@ import SnapKit
 import Then
 
 final class ActiveViewController: BaseViewController {
+    
     private let rootView = ActiveView()
+    
+    private let artistDetailService: ArtistDetailService = DefaultArtistDetailService()
     
     override func loadView() {
         view = rootView
@@ -20,5 +23,20 @@ final class ActiveViewController: BaseViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func setView() {
+        fetchArtistDetail()
+    }
+    
+    private func fetchArtistDetail() {
+        Task {
+            do {
+                let response = try await artistDetailService.getArtistDetail(artistId: 13)
+                rootView.configureArtistDetail(response)
+            } catch {
+                print("아티스트 상세정보 조회 실패: \(error)")
+            }
+        }
     }
 }
